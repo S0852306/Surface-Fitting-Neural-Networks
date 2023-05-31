b8e4332b6b3068a42fad45f1743946e68c365359
S0852306 <111946393+S0852306@users.noreply.github.com>
2023-05-31 15:13:06 +0800

    Update CostFunction.m

--- CostFunction.m
+++ CostFunction.m
@@ -1,10 +1,6 @@
 function FunctionOutput=CostFunction(data,label,NN)
+
 Cost=NN.Cost;
-
-if strcmp(NN.InputAutoScaling,'on')==1
-    data=NN.InputScaleVector.*data-NN.InputCenterVector;
-end
-
 
 NetworkType=NN.NetworkType;
 switch NetworkType
