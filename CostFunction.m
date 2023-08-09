66a688c3e5eb1688ba4fbb9b2f06d4f3d71b2475
S0852306 <111946393+S0852306@users.noreply.github.com>
2023-08-09 22:51:00 +0800

    Update CostFunction.m

--- CostFunction.m
+++ CostFunction.m
@@ -9,17 +9,35 @@ switch NetworkType
 end
 
 predict=Net(data,NN);
+if size(label,2)==NN.numOfData && NN.WeightedFlag==1
+    DataWeightMatrix=NN.Weighted;
+elseif size(label,2)~=NN.numOfData && NN.WeightedFlag==1
+    DataWeightMatrix=NN.SampleWeight;
+end
 
 switch Cost
     case 'SSE'
-        temp=(label-predict).^2;
+        if isfield(NN,'Weighted')~=1
+            temp=(label-predict).^2;
+        else
+            temp=DataWeightMatrix.*(label-predict).^2;
+        end
         E=sum(temp,[1 2]);
     case 'MSE'
-        temp=(label-predict).^2;
+        if isfield(NN,'Weighted')~=1
+            temp=(label-predict).^2;
+        else
+            temp=DataWeightMatrix.*(label-predict).^2;
+        end
         E=NN.MeanFactor*sum(temp,[1 2]);
     case 'MAE'
-        temp=abs(label-predict);
+        if isfield(NN,'Weighted')~=1
+            temp=abs(label-predict);
+        else
+            temp=DataWeightMatrix.*abs(label-predict);
+        end
         E=NN.MeanFactor*sum(temp,[1 2]);
+        
     case 'Entropy'
         temp=-label.*log(max(predict,1e-8));
         E=NN.MeanFactor*sum(temp,[1 2]);
